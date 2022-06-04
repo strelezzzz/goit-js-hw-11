9eabb13557f3419717fe96153d5a548ea0120f63
Taras Bondar <strelezzzz@gmail.com>
2022-06-05 00:35:01 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import './sass/main.scss';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
+
+EndOfSearchMessage();
+noImageMessage();
+
+function noImageMessage() {
+  Notify.info('Sorry, there are no images matching your search query. Please try again.');
+}
 function EndOfSearchMessage() {
   Notify.info("We're sorry, but you've reached the end of search results.");
 }
-
-EndOfSearchMessage();
