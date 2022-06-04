19216cff9cf79b6e2e4164e5b805eb390d47a1dd
Taras Bondar <strelezzzz@gmail.com>
2022-06-05 00:03:15 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.scss';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+function EndOfSearchMessage() {
+  Notify.info("We're sorry, but you've reached the end of search results.");
+}
+
+EndOfSearchMessage();
