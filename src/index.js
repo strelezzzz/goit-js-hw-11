bc2dd7988ad0ed255da9522fa153b86e80f26428
Taras Bondar <strelezzzz@gmail.com>
2022-06-05 17:58:34 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import './sass/main.scss';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 
+import GetImages from './js/query-service';
+// const images = new GetImages();
+
+// import * as GetImages from '.js/query-service';
+
+const form = document.querySelector('.search-form');
+console.log(searchForm);
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+}
+
 EndOfSearchMessage();
 noImageMessage();
 
